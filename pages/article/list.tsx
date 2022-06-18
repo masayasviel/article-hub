5beba6db2a0411dff2c70bd43d3bb39604753bee
moshi10 <mamiko.oshino@gmail.com>
2022-06-19 03:55:27 +0900

    一応完成

--- pages/article/list.tsx
+++ pages/article/list.tsx
@@ -3,26 +3,36 @@ import type { NextPage } from 'next';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import ArticleColumn from '@organization/article-column/article-column';
+import { Box, Container } from '@mui/system';
 
-const testData = [
-    {author:"いなくん",title:"dockerについて"},
-    {author:"ぞのりょー",title:"next.js+typesciptでいろいろ"},
-    {author:"masaya",title:"希望者がいればって感じです"}
-]
 
+// style={{align-items:'flex-end', display:'flex', justify-content:'flex-end'}}
 const ArticleList: NextPage = () => {
-    const [count, setCount] = useState(0)
-    const handleChoice = () => {
-        setCount(count + 1);
-        incNumber();
+    const testData = [
+        { author: "yamada", title: "dockerについて" },
+        { author: "hanako", title: "next.js+typesciptでいろいろ" },
+        { author: "taroooou", title: "浮動小数点と固定小数点" }
+    ]
+    const rightStyles ={
+        display: 'flex',
+        aliginItems: 'flex-end',
+        justifyContent: 'flex-end'
     }
-
-    return(
+    return (
         <>
-            <p>あーてぃくるりすと</p>
-            <ArticleColumn  ArticleColumnProps={testData[]}/>
-        
-
+            <Container maxWidth="lg">
+                <h1>Article List</h1>
+                <Container style={rightStyles}>
+                    <Button>記事作成</Button>
+                </Container>
+                <Stack spacing={5} margin={3}>
+                    {testData.map((item, index) => {
+                        return (
+                            <ArticleColumn key={index} title={item.title} author={item.author} />
+                        )
+                    })}
+                </Stack>
+            </Container>
         </>
     )
 }
--- src/components/organization/article-column/article-column.tsx
+++ src/components/organization/article-column/article-column.tsx
@@ -1,41 +1,30 @@
-import { Card, CardContent, Typography, CardActions, Button, Stack, Divider } from "@mui/material";
+import { Card, CardContent, Typography, Stack, Divider, CardActionArea } from "@mui/material";
 
 interface ArticleColumnProps {
     title: string;
     author: string;
 }
 
-const ArticleColumn = ({title,author}: ArticleColumnProps) => {
-    
-
+const ArticleColumn = ({ title, author }: ArticleColumnProps) => {
     return (
         <>
-
             <Card sx={{ minWidth: 275 }}>
-                <Stack
-                    direction="row"
-                    divider={<Divider orientation="vertical" flexItem />}
-                    spacing={2}
-
-                >
-                    <CardActions>
-                        <Button size="small">emoji</Button>
-                    </CardActions>
-                    <CardContent>
-                        <Typography variant="h5" component="div">
-                            {title}
-                        </Typography>
-                        <Stack direction="row">
+                <CardActionArea>
+                    <Stack
+                        direction="row"
+                        divider={<Divider orientation="vertical" flexItem />}
+                    >
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                {title}
+                            </Typography>
                             <Typography sx={{ mb: 1.5 }} color="text.secondary">
                                 {author}
                             </Typography>
-                        </Stack>
-                    </CardContent>
-                </Stack>
+                        </CardContent>
+                    </Stack>
+                </CardActionArea>
             </Card>
-
-
-
         </>
     )
 }
