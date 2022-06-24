cc9dd0125fff2b174e99fc8bd0aae727d6fd5c7e
masayasviel <33654883+masayasviel@users.noreply.github.com>
2022-06-25 02:45:24 +0900

    #5 functionをserviceに統一

--- src/services/formControl.service.ts
+++ src/services/formControl.service.ts
@@ -1,7 +1,4 @@
 import { ChangeEvent } from 'react';
 
-// 共通で使えるtsのコード
-
 /** フォームの入力値を取得*/
 export const getInputForValue = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): string => event.target.value;
-
