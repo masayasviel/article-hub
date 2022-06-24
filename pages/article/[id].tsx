d9d670cffd0cb510c2e3138e1c8c386754549910
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-06-24 22:27:31 +0900

    create detail page v1.0

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -1,26 +1,11 @@
+import Content from "@organization/content/content";
 import { NextPage } from "next";
-import styles from '../../components/layout.module.css';
-import Link from 'next/link';
-import ContentDisplayPaper from "@organization/content/contentDisplayPaper";
 
-const ContentPage: NextPage = ({ children }: any) => {
+const ContentPage: NextPage = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <h1 className={styles.articleTitle}>Article Title</h1>
-        <div className={styles.articleBody}>
-          <div className={styles.backToHome}>
-            <Link href="/">
-              <a>← Back to home</a>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.container}>
-          <ContentDisplayPaper />
-          <main>{children}</main>
-        </div>
-      </div>
-    </>
+      <>
+        <Content />
+      </>
   );
 }
 
