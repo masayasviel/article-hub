fb10f314c3832a4b376bc4e53c8fc062d0d2abb6
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-08-12 23:47:07 +0900

    記事詳細のReadAPI作成v1.0

--- src/controller/article/content.ts
+++ src/controller/article/content.ts
@@ -1,1 +1,26 @@
-// ぞのりょー
+import { PrismaClient } from '@prisma/client'
+
+import { NotFoundError } from "@src/controller/core/exception";
+
+/**
+ * 記事取得情報
+ * @param articleId 記事ID
+ * @exception {NotFoundError} 記事が存在しない
+ */
+
+//記事詳細を取得する
+export async function getArticle(
+    articleId: number,
+): Promise<void> {
+    const prisma = new PrismaClient();
+
+    const target = await prisma.post.findUnique({
+        where: {
+            id: articleId,
+        }
+    });
+    if (target === null) {
+        throw new NotFoundError('post id');
+    }
+    await prisma.$disconnect();
+}
