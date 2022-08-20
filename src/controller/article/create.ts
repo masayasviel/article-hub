d2078c3abc0750ec5539ec298165f432e3b91737
inakuuun <bkg.inamoto@gmail.com>
2022-08-20 23:32:27 +0900

    いなくん:記事登録作成

--- src/controller/article/create.ts
+++ src/controller/article/create.ts
@@ -1,1 +1,24 @@
-// いなくん
+import { PrismaClient } from '@prisma/client'
+
+import { CreateArticleRequestType } from "@interfaces/article.interface";
+
+/**
+ * 記事登録
+ * @param requestParam 登録内容
+ */
+export async function create(
+    requestParam: CreateArticleRequestType
+): Promise<void> {
+    // prismaをインスタンス化
+    const prisma = new PrismaClient();
+
+    await prisma.post.create({
+        data: {
+            userid: 1,
+            title: requestParam.title,
+            text: requestParam.content,
+        },
+    });
+
+    await prisma.$disconnect();
+}
